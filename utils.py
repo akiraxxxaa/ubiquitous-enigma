e344c1a2486bf2aed1e683ea258f8e925d3b9f22
akiraxxxaa <akiraxxxaa@users.noreply.github.com>
2025-10-28 09:15:00 +0200

    Add utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+"""
+Utility functions.
+"""
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def process_data(data):
+    """Process input data."""
+    return data.upper()
